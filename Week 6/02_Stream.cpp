ccbf9575b87512c1d3b405eb8a1a0594d1a12ef3
Ahmedg66 <142092855+Ahmedg66@users.noreply.github.com>
2024-03-11 14:41:36 +0200

    Update 02_Stream.cpp

--- Week 6/02_Stream.cpp
+++ Week 6/02_Stream.cpp
@@ -1,102 +1,116 @@
-#include <iostream>
-#include <cassert>
+#include<iostream>
+#include<cassert>
 using namespace std;
-
-class Queue {
-  int size { };
-  int front { 0 };
-  int rear { 0 };
-  int added_elements { };
-  int *array { };
-
-public:
-  Queue(int size) :
-      size(size) {
-    array = new int[size];
-  }
-
-  ~Queue() {
-    delete[] array;
-  }
-
-  int next(int pos) {
-    ++pos;
-    if (pos == size)
-      pos = 0;
-    return pos;
-  }
-
-  void enqueue(int value) {
-    assert(!isFull());
-    array[rear] = value;
-    rear = next(rear);
-    added_elements++;
-  }
-
-  int dequeue() {
-    assert(!isEmpty());
-    int value = array[front];
-    front = next(front);
-    --added_elements;
-    return value;
-  }
-
-  void display() {
-    cout << "Front " << front << " - rear " << rear << "\t";
-    if (isFull())
-      cout << "full";
-    else if (isEmpty()) {
-      cout << "empty\n\n";
-      return;
-    }
-    cout << "\n";
-
-    for (int cur = front, step = 0; step < added_elements; ++step, cur = next(cur))
-      cout << array[cur] << " ";
-    cout << "\n\n";
-  }
-
-  int isEmpty() {
-    return added_elements == 0;
-  }
-
-  bool isFull() {
-    return added_elements == size;
-  }
-};
-
-class Last_k_numbers_sum_stream {
-private:
-  int k;
-  int sum {};
-  int added_elements {};
-  Queue q;
-
-public:
-  Last_k_numbers_sum_stream(int k) :
-      k(k), q(k+1) {
-  }
-
-  int next(int new_num) {
-    q.enqueue(new_num);
-    added_elements++;
-    sum += new_num;
-
-    if (added_elements > k) {
-      sum -= q.dequeue();
-      added_elements--;
+class queue
+{
+    private :
+    int add_elements=0;
+    int front=0;
+    int rear=0;
+    int *arr;
+    int size;
+    
+    public:
+    queue(int _size)
+    {
+        size=_size;
+        arr=new int[size];
     }
 
-    return sum;
-  }
+    int next(int pos)
+    {
+        pos++;
+        if(pos==size)
+        return 0;
+        return pos;
+    }
+
+    
+
+   
+    int isfull()
+    {
+        return add_elements==size;
+    }
+    int empty()
+    {
+    return add_elements==0;
+    }
+
+    void enqueue(int val)
+    {
+        assert(!isfull());
+        arr[rear]=val;
+        rear=next(rear);
+        add_elements++;
+    }
+
+  
+
+    int dequeue()
+    {
+        assert(!empty());
+        int val=arr[front];
+        front=next(front);
+        add_elements--;
+        return val;
+    }
+    
+    void display()
+    {
+        for(int cur=front;cur!=rear;cur=next(cur))
+        cout << arr[cur] << " ";
+    }
+
+    int peek()
+{
+    return arr[front];
+}
+
+int addelements()
+{
+    return add_elements;
+}
+
+};
+class sum_last_k
+{
+    private :
+    int sum=0;
+    int k;
+    queue q;
+
+    public :
+    sum_last_k(int _k):q(_k)
+    {  
+        k=_k;
+    }
+
+    int next(int new_num)
+    {
+        sum+=new_num;
+        if(q.addelements()==k){
+            int deq=q.dequeue();
+            sum-=deq;
+        }
+        q.enqueue(new_num);
+        return sum;
+     
+
+
+        
+    }
+    
+
 };
 
-int main() {
-  Last_k_numbers_sum_stream processor(4);
 
-  int num;
-  while (cin >> num)
-    cout << processor.next(num) << "\n";
-
-  return 0;
+int main()
+{
+    sum_last_k process(4);
+    int num;
+    while(cin >> num)
+    {
+        cout << process.next(num) << endl;
+    }
 }
